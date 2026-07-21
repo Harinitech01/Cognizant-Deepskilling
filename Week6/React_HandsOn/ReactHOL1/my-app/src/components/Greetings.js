9669124acaa5e5243c23e69a07a0f2c40518e622
Harinitech01 <harinisrij01@gmail>
2026-07-21 11:41:40 +0530

    Completed Week 5 Microservices and Week 6 React Hands-on 1

---
+++ Week6/React_HandsOn/ReactHOL1/my-app/src/components/Greetings.js
@@ -0,0 +1,11 @@
+function Greeting(props) {
+  return (
+    <div>
+      <h2>Hello, {props.name}!</h2>
+      <p>Welcome to React Components.</p>
+      <hr />
+    </div>
+  );
+}
+
+export default Greeting;
